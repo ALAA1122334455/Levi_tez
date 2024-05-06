db0bf052cc67cb4f65a6aa98f0438a4726a56a00
ALAA1122334455 <106932960+ALAA1122334455@users.noreply.github.com>
2024-05-06 11:53:43 +0300

    Update mainn.py

--- mainn.py
+++ mainn.py
@@ -7,7 +7,7 @@ from user_agent import generate_user_agent
 
 import httpx
 
-apps = Flask(__name__)
+app = Flask(__name__)
 
 def FromCreate(email):
     try:
@@ -46,4 +46,5 @@ def FromCreate(email):
 def check_email(email):
     result = FromCreate(email)
     return jsonify(result)
-
+if __name__ == '__main__':
+    app.run()
